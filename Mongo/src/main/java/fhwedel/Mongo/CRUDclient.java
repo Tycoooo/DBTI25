51a85650e6c319353db682f589e45a29893a0c83
Tycoooo <Speedy108@team-menge.de>
2025-07-02 13:35:49 +0000

    Aufgabe 2 fertig + Firma angefangen

--- Mongo/src/main/java/fhwedel/Mongo/CRUDclient.java
+++ Mongo/src/main/java/fhwedel/Mongo/CRUDclient.java
@@ -7,6 +7,14 @@ import org.bson.Document;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Updates;
 
 public class CRUDclient {
     public static void main(String[] args) {
@@ -16,15 +24,18 @@ public class CRUDclient {
         MongoCollection<Document> readers = db.getCollection("leser");
         MongoCollection<Document> loans = db.getCollection("entliehen");
 
+        //Marc-Uwe hinzufügen
         books.insertOne(new Document("invr", 1)
                 .append("autor", "Marc-Uwe Kling")
                 .append("titel", "Die Känguru-Chroniken: Ansichten eines vorlauten Beuteltiers")
                 .append("verlag", "Ulstein-Verlag"));
 
+        //Friedrich Funke hinzufügen
         readers.insertOne(new Document("lnr", 1)
                 .append("name", "Friedrich Funke")
                 .append("adresse", "Bahnhofstraße 17, 23758 Oldenburg"));
 
+        //5 Leser hinzufügen
         List<Document> readerDocs = Arrays.asList(
                 new Document(Map.of(
                         "lnr", 2,
@@ -48,6 +59,7 @@ public class CRUDclient {
                         "adresse", "Konoha")));
         readers.insertMany(readerDocs);
 
+        //5 Bücher hinzufügen
         List<Document> booksDocs = Arrays.asList(
                 new Document(Map.of(
                         "invr", 2,
@@ -77,14 +89,122 @@ public class CRUDclient {
 
         books.insertMany(booksDocs);
 
-        Document doc = books.find(Filters.eq("autor", "Marc-Uwe Kling")).first();
+        //Person leiht Buch 
+        List<Document> loansDocs = Arrays.asList(
+                new Document(Map.of(
+                       "lnr", 2,
+                        "invr", 2,
+                        "returnDate", new GregorianCalendar(2025, Calendar.JULY, 2).getTime()
+                )),
+                new Document(Map.of(
+                        "lnr", 2,
+                        "invr", 3,
+                        "returnDate", new GregorianCalendar(2026, Calendar.JUNE, 5).getTime()
+                )),
+                new Document(Map.of(
+                        "lnr", 4,
+                        "invr", 5,
+                        "returnDate", new GregorianCalendar(2026, Calendar.JUNE, 5).getTime()
+                )),
+                new Document(Map.of(
+                        "lnr", 4,
+                        "invr", 4,
+                        "returnDate", new GregorianCalendar(2026, Calendar.JUNE, 5).getTime()
+                ))
+                
+        );
+        loans.insertMany(loansDocs);
 
-        System.out.println("2b) Bücher von Marc-Uwe Kling: \"" + doc.getString("titel") + '"');
+        Document result_2b = books.find(Filters.eq("autor", "Marc-Uwe Kling")).first();
+
+        System.out.println("2b) Bücher von Marc-Uwe Kling: \"" + result_2b.getString("titel") + '"');
         System.out.println("2c) Anzahl an Büchern in Bibliothek: " + books.countDocuments());
+
+        AggregateIterable<Document> result_2d = loans.aggregate(Arrays.asList(
+                    Aggregates.group("$lnr", Accumulators.sum("anzahlBuecher", 1)),
+                    Aggregates.match(Filters.gt("anzahlBuecher", 1)),
+                    Aggregates.sort(Sorts.descending("anzahlBuecher"))
+                ));
+
+        // Ausgabe 2d
+        System.out.println("2d) Namen der Leser mit mindestens 2 ausgeliehenen Büchern:");
+        for (Document doc : result_2d) {
+                String name = readers.find(Filters.eq("lnr", doc.get("_id"))).first().getString("name");
+                Integer numberOfBooks = doc.getInteger("anzahlBuecher");
+                System.out.println("    Leser: " + name + ", Bücher ausgeliehen: " + numberOfBooks);
+        }
+
+        //2e Friedrich Funke leiht Känguru-Chroniken aus
+        loans.insertOne(
+                new Document(Map.of("lnr", 1, "invr", 1, "returnDate", new GregorianCalendar(2026, Calendar.JUNE, 5).getTime()))
+        );
+
+        //2e Friedrich Funke gibt Känguru-Chroniken zurück
+        loans.deleteOne(Filters.and(
+                Filters.eq("lnr", 1),
+                Filters.eq("invr", 1)
+        ));
+
+        //2f Heinz Müller leiht 2 Bücher aus
+        /*
+        Vorteile:
+                - schneller Zugriff (alles in einem Dokument, kein Join notwendig)
+                - einfache Struktur (nicht so stark verschachtelt)
+        Nachteile:
+                - schwere Wartung (bei Änderung des Verlags muss dieser überall geändert werden)
+                - schwierige Abfrage (von wem wird bestimmtes Buch ausgeliehen -> alle Leser durchgehen)
+        */
+        Document heinzMueller = new Document(Map.of(
+                "lnr", 99,
+                "name", "Heinz Müller",
+                "adresse", "Klopstockweg 17, 38124 Braunschweig",
+                "loans", Arrays.asList(
+                        new Document(Map.of(
+                                "invr", 1,
+                                "titel", "Die Känguru-Chroniken",
+                                "autor", "Marc-Uwe Kling",
+                                "verlag", "Ulstein-Verlag"
+                        )),
+                        new Document(Map.of(
+                                "invr", 7,
+                                "titel", "Der König von Berlin",
+                                "autor", "Horst Evers",
+                                "verlag", "Rowohlt-Verlag"
+                        ))
+                )
+        ));
+
+        readers.insertOne(heinzMueller);
+
+        //2g Heinz Müller gibt Känguru-Chroniken zurück
+        readers.updateOne(
+            Filters.eq("lnr", 99),
+            Updates.pull("loans", new Document("invr", "1"))
+        );
+
+        //2g Friedrich Funke leiht Känguru-Chroniken aus
+        readers.updateOne(
+            Filters.eq("lnr", 1),
+            Updates.push("loans", new Document()
+                    .append("invr", 1)
+                    .append("titel", "Die Känguru-Chroniken")
+                    .append("autor", "Marc-Uwe Kling")
+                    .append("verlag", "Ulstein-Verlag"))
+        );
 
         books.drop();
         readers.drop();
         loans.drop();
+
+        db = mongoClient.getDatabase("firma");
+        MongoCollection<Document> personal = db.getCollection("personal");
+        MongoCollection<Document> gehalt = db.getCollection("gehalt");
+        MongoCollection<Document> abteilung = db.getCollection("abteilung");
+        MongoCollection<Document> kind = db.getCollection("kind");
+        MongoCollection<Document> maschine = db.getCollection("maschine");
+        MongoCollection<Document> praemie = db.getCollection("praemie");
+
+
         mongoClient.close();
         System.exit(0);
     }
